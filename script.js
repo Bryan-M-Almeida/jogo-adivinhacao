e99b7dcbefce0300213892b1dc476565dae205fe
Bryan Maciel <bryanmacieldealmeida06@gmail.com>
2025-05-13 17:29:16 -0300

    adicionado sistema de pontuação e vida

---
+++ script.js
@@ -0,0 +1,79 @@
+/* Vida */
+const vida = document.querySelector(".vida");
+vida.innerHTML = 100;
+
+/* pergunta */
+const questao = document.querySelector(".pergunta");
+
+/* respostas */
+const respostas = document.querySelectorAll(".resposta");
+
+/* pontuação */
+const pontos = document.querySelector(".pontos");
+
+/* contador */
+const acertos = 0;
+
+/* questão */
+let questaoIndex = 0;
+
+
+
+fetch('jogo.json')
+    .then(response => response.json())
+    .then(data => {
+        carregarQuestao(questaoIndex, data);
+
+        respostas.forEach(botao => {
+            botao.addEventListener("click", function () {
+                const clicado = this.innerText;
+                const respostaCorreta = data.questoes[questaoIndex].resposta;
+
+                if (vida.innerHTML > 0) {
+                    if (clicado === respostaCorreta) {
+                        this.classList.add("certa");
+                        let score = Math.floor(Math.random() * 10)
+                        let vidaAtual = parseInt(vida.innerHTML);
+
+                        let pontoAtual = parseInt(pontos.innerHTML);
+                        pontoAtual += score;
+                        pontos.innerHTML = pontoAtual;
+                        if (vidaAtual > 0 && vidaAtual < 66) {
+
+
+                            vidaAtual += 35;
+                            vida.innerHTML = vidaAtual;
+                        }
+                        respostas.forEach(btn => {
+                            btn.disabled = true;
+                        });
+                        questaoIndex++;
+                        setTimeout(() => carregarQuestao(questaoIndex, data), 1000);
+                    }
+
+
+                    else {
+                        this.classList.add("errada");
+                        this.disabled = true;
+                        let vidaAtual = parseInt(vida.innerHTML);
+                        vidaAtual -= 35;
+                        vida.innerHTML = vidaAtual > 0 ? vidaAtual : 0;
+                    }
+                }
+            });
+        });
+    });
+
+function carregarQuestao(index, data) {
+    questao.innerHTML = data.questoes[index].pergunta;
+    respostas.forEach((element, i) => {
+        element.innerHTML = data.questoes[index].alternativas[i];
+        element.classList.remove("certa", "errada");
+        element.disabled = false;
+    });
+}
+
+
+setInterval(() => {
+    document.querySelector('.vida').style.width = vida.innerHTML + "%";
+}, 0);
